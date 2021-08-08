baab9e82dd1345009f29dac2c3b0a359dd10daf2
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2021-08-08 11:48:24 +0545

    Add urls.py file

--- kitabkhana/urls.py
+++ kitabkhana/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.urls.conf import include
 
-def function(request):
-    return render(request,"html/base.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',function)
+    path('',include('authentication.urls')),
+    path('',include('books.urls')),
+    path('',include('chart.urls')),
+
 ]
