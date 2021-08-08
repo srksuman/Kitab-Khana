9da6c6c97c11fba0dcb2b6c4623141a77ee9d0e6
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2021-08-08 12:20:42 +0545

    Added url for login and register, logic to get register and login

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from. import views
 urlpatterns = [
-    
+    path('',views.loginFunction,name="login"),
+    path('register/',views.registerFunction,name="register")
+
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -11,29 +11,36 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 # Create your views here.
 def registerFunction(request):
-    if request.method =="POST":
-            form = UserCreation(request.POST)
-            if form.is_valid():
-                form.save()
-                messages.success(request,"Account is created successfully!")
-                return HttpResponseRedirect('')
-            else:
-                log = LoginForm(request = request,data = request.POST)
+    if not request.user.is_authenticated:
+        if request.method =="POST":
+                form = UserCreation(request.POST)
+                if form.is_valid():
+                    form.save()
+                    messages.success(request,"Account is created successfully!")
+                    return HttpResponseRedirect('')
+                else:
+                    log = LoginForm(request = request,data = request.POST)
 
-    else:  
-        forms = UserCreation()
-    return render(request,"html/auth/register.html",{'form':form})
+        else:  
+            form = UserCreation()
+        return render(request,"html/auth/register.html",{'form':form})
+    else:
+        return HttpResponse("hello")
 
 def loginFunction(request):
-    if request.method == 'POST':
-        form = LoginForm(request = request,data = request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            pas = form.cleaned_data['password']
-            usr = authenticate(username=username,password = pas)
-            login(request,usr)
-            return HttpResponse("Login success")
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = LoginForm(request = request,data = request.POST)
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                pas = form.cleaned_data['password']
+                usr = authenticate(username=username,password = pas)
+                login(request,usr)
+                messages.success(request,"Account is created successfully!")
+                return HttpResponse("Login success")
+        else:
+            form = LoginForm()
+        return render(request,"html/auth/login.html",{'form':form})
     else:
-        log = LoginForm()
-    return render(request,"html/auth/login.html",{'form':form})
+        return HttpResponse("Login")
 
