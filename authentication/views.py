7de80ecbfafed8a29ce2c080c69b8eea056e81f7
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2021-08-08 12:06:51 +0545

    templates files added register.html and login.html, logic for register

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.contrib import auth
+from django.contrib.auth import forms
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from .forms import UserCreation,LoginForm
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 # Create your views here.
+def registerFunction(request):
+    if request.method =="POST":
+            form = UserCreation(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request,"Account is created successfully!")
+                return HttpResponseRedirect('')
+            else:
+                log = LoginForm(request = request,data = request.POST)
+
+    else:  
+        forms = UserCreation()
+    return render(request,"html/auth/register.html",{'form':form})
+
+
+
