2efd0fcae37544e864ad39c979c335061f349059
Ashmita Devkota <devkota9803@gmail.com>
2021-08-20 17:58:52 +0545

    Front-end of register page

--- kitabkhana/urls.py
+++ kitabkhana/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -5,6 +6,7 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('registerfrontend.urls')),
     path('',include('authentication.urls')),
     path('',include('books.urls')),
     path('',include('chart.urls')),
