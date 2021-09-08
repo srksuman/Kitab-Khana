bf756bb58a91cde3cb7a7b489f82c4f0d47fde90
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2021-09-08 19:54:39 +0545

    Generating colours function added

--- chart/views.py
+++ chart/views.py
@@ -1,7 +1,7 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from books.models import UploadsBook
-from django.db.models import Q
+import random
 # Create your views here.
 #  get_type_book_and_qty_number
 def bookAndQty():
@@ -13,7 +13,18 @@ def bookAndQty():
         else:
             name_qty[i.Types_of_Book] += i.Quantity
     return name_qty
-    
+
+
+#parameter 'l' => length of the dictonary returned form the bookAndQty function 
+def generating_different_color_function(l):
+    rgb_list = []
+    for _ in range(l):
+        r = str(random.randint(0,256))
+        g = str(random.randint(0,256))
+        b = str(random.randint(0,256))
+        rgb = f'rgb({r},{g},{b})'
+        rgb_list.append(rgb)
+    return rgb_list
 
 def showChart(request):
     name_qty = bookAndQty()
