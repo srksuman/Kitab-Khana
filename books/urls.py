2b1b5402cd29efe26a85305313e3e417fc5a2cdd
SamratBogati <samratbogati4@gmail.com>
2021-09-26 21:11:03 +0545

    Creating dynamic url for delete and update page

--- books/urls.py
+++ books/urls.py
@@ -4,10 +4,10 @@ from books import views
 
 
 urlpatterns = [
-    path('contact/',views.contact, name="contact"),
-    path('addproduct/',views.addproduct, name="addproduct"),
-    path('add/',views.add, name="add"),
-    path('view/',views.view, name="view")
-   
-    
+    path('contact/', views.contact, name="contact"),
+    path('addproduct/', views.addproduct, name="addproduct"),
+    path('add/', views.add, name="add"),
+    path('view/', views.view, name="view"),
+    path('delete/<int:id>/', views.delete_book, name="deletebook"),
+    path('update/<int:id>/', views.update_book, name="updatebook")
 ]
